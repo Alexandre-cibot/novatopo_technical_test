805a1ccbfbfdda8ae07b574a74c6e1b3af98f292
Alexandre Cibot <alexandre.cibot@gmail.com>
2018-05-19 12:50:52 +0200

    Adding asyncronous

--- index.js
+++ index.js
@@ -14,21 +14,35 @@ async function getInstructions() {
   const instructions = normalizeInstructions(await getInstructions());
   const garden = instructions.garden
   let mowers = instructions.mowers.map(mowerInstruction => new Mower(mowerInstruction));
-  mowers.forEach(mower => console.log(start(mower)));
+
+  let mowersLaunched = [];
+
+  mowers.forEach((mower, i) => {
+    console.log('Lancement de mower', i)
+    mowersLaunched.push(start(mower));
+  });
+
+  mowersLaunched.forEach(promise => {
+    // When a promise a resolved, show result.
+    promise.then(console.log);
+  })
+  
 })()
 
 
 
 function start(mowerInstance){
   // on lance la boucle des orders données.
-	mowerInstance.orders.forEach(order => {
-		if (isADirection(order)) {
-      // L'order est une direction, on change l'orientation en prenant en compte l'ancienne.
-      mowerInstance.updateOrientation(order);
-		} else {
-    // L'order est un mouvement, on avance d'un point dans cette direction de notre orientation.
-    mowerInstance.updatePosition();
-		}
+  return new Promise((resolve, reject) => {
+    mowerInstance.orders.forEach(order => {
+      if (isADirection(order)) {
+        // L'order est une direction, on change l'orientation en prenant en compte l'ancienne.
+        mowerInstance.updateOrientation(order);
+      } else {
+      // L'order est un mouvement, on avance d'un point dans cette direction de notre orientation.
+      mowerInstance.updatePosition();
+      }
+    })
+    resolve(mowerInstance.getInformations());
   })
-  return mowerInstance.getInformations();
 }
